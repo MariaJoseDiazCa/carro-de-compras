c10542788f83b599d3f9ed15f88b3b29aa87cdf9
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2021-11-11 12:33:23 -0300

    ADD: Button component

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import Button from "./Button";
 
 const styles = {
   producto: {
@@ -15,12 +16,15 @@ const styles = {
 
 class Producto extends Component {
   render() {
-    const { producto } = this.props;
+    const { producto, agregarAlCarro } = this.props;
     return (
       <div style={styles.producto}>
         <img alt={Producto.name} src={producto.img} />
         <h3>{producto.name}</h3>
         <p>{producto.price}</p>
+        <Button onClick={() => agregarAlCarro(producto)}>
+          Agregar al carro
+        </Button>
       </div>
     );
   }
