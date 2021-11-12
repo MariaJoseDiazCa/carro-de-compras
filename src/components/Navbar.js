4493b494b1981a613829895fc825ca987ec7174e
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2021-11-12 17:36:59 -0300

    ADD: Car component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import Logo from "./Logo";
+import Car from "./Car";
 
 const styles = {
   navbar: {
@@ -19,7 +20,7 @@ class Navbar extends Component {
     return (
       <nav style={styles.navbar}>
         <Logo />
-        <p>Carro</p>
+        <Car />
       </nav>
     );
   }
